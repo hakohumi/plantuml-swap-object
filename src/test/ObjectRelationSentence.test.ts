e23b920396e03ab5650c8092bcbd028d61bd6d09
filu <mihumihu32@gmail.com>
2025-01-13 18:27:26 +0900

    ✨ refactor tests to use input_string and add support for comments in object relation parsing

--- src/test/ObjectRelationSentence.test.ts
+++ src/test/ObjectRelationSentence.test.ts
@@ -5,96 +5,128 @@ import {
 } from '../ObjectRelationSentence'
 
 type TestPattern = {
-  actual: string
+  input_string: string
   expect: ObjectRelationSentence | null
 }
 
-const test_swap_object_patterns: TestPattern[] = [
-  {
-    actual: 'A --> B',
-    expect: {
-      left_bol: '',
-      left_object: 'A',
-      left_empty: ' ',
-      left_arrow: '',
-      line: '--',
-      right_arrow: '>',
-      right_empty: ' ',
-      right_object: 'B',
-      right_eol: '',
-    },
-  },
-  {
-    actual: 'A <|. B',
-    expect: {
-      left_bol: '',
-      left_object: 'A',
-      left_empty: ' ',
-      left_arrow: '<|',
-      line: '.',
-      right_arrow: '',
-      right_empty: ' ',
-      right_object: 'B',
-      right_eol: '',
-    },
-  },
-  {
-    actual: 'A .. B',
-    expect: {
-      left_bol: '',
-      left_object: 'A',
-      left_empty: ' ',
-      left_arrow: '',
-      line: '..',
-      right_arrow: '',
-      right_empty: ' ',
-      right_object: 'B',
-      right_eol: '',
-    },
-  },
-  {
-    actual: 'A o--o B',
-    expect: {
-      left_bol: '',
-      left_object: 'A',
-      left_empty: ' ',
-      left_arrow: 'o',
-      line: '--',
-      right_arrow: 'o',
-      right_empty: ' ',
-      right_object: 'B',
-      right_eol: '',
-    },
-  },
-  {
-    actual: 'A ---* B',
-    expect: {
-      left_bol: '',
-      left_object: 'A',
-      left_empty: ' ',
-      left_arrow: '',
-      line: '---',
-      right_arrow: '*',
-      right_empty: ' ',
-      right_object: 'B',
-      right_eol: '',
-    },
-  },
-]
-
-function test_swap_object_success(
-  input_string: string,
-  expect: ObjectRelationSentence | null
-) {
-  const actual = parse_object_relation(input_string)
-
-  assert.deepEqual(actual, expect)
-}
-
-suite('Swap Object Test Suite', () => {
+suite('parse_object_relation Test Suite', () => {
   test('success test', () => {
-    test_swap_object_patterns.forEach(({ actual, expect }) => {
-      test_swap_object_success(actual, expect)
+    const test_success_patterns: TestPattern[] = [
+      {
+        input_string: 'A --> B',
+        expect: {
+          left_bol: '',
+          left_object: 'A',
+          left_empty: ' ',
+          left_arrow: '',
+          line: '--',
+          right_arrow: '>',
+          right_empty: ' ',
+          right_object: 'B',
+          right_eol: '',
+        },
+      },
+      {
+        input_string: 'A <|. B',
+        expect: {
+          left_bol: '',
+          left_object: 'A',
+          left_empty: ' ',
+          left_arrow: '<|',
+          line: '.',
+          right_arrow: '',
+          right_empty: ' ',
+          right_object: 'B',
+          right_eol: '',
+        },
+      },
+      {
+        input_string: 'A .. B',
+        expect: {
+          left_bol: '',
+          left_object: 'A',
+          left_empty: ' ',
+          left_arrow: '',
+          line: '..',
+          right_arrow: '',
+          right_empty: ' ',
+          right_object: 'B',
+          right_eol: '',
+        },
+      },
+      {
+        input_string: 'A o--o B',
+        expect: {
+          left_bol: '',
+          left_object: 'A',
+          left_empty: ' ',
+          left_arrow: 'o',
+          line: '--',
+          right_arrow: 'o',
+          right_empty: ' ',
+          right_object: 'B',
+          right_eol: '',
+        },
+      },
+      {
+        input_string: 'A ---* B',
+        expect: {
+          left_bol: '',
+          left_object: 'A',
+          left_empty: ' ',
+          left_arrow: '',
+          line: '---',
+          right_arrow: '*',
+          right_empty: ' ',
+          right_object: 'B',
+          right_eol: '',
+        },
+      },
+      {
+        input_string: 'A - B',
+        expect: {
+          left_bol: '',
+          left_object: 'A',
+          left_empty: ' ',
+          left_arrow: '',
+          line: '-',
+          right_arrow: '',
+          right_empty: ' ',
+          right_object: 'B',
+          right_eol: '',
+        },
+      },
+    ]
+
+    test_success_patterns.forEach(({ input_string, expect }) => {
+      const actual = parse_object_relation(input_string)
+      assert.deepEqual(actual, expect)
     })
+  })
+
+  test('コメント用のコロンが含まれているテストパターン', () => {
+    const test_pattern_containing_a_colon_for_comments: TestPattern[] = [
+      {
+        input_string: 'A --> B : ABCD',
+        expect: {
+          left_bol: '',
+          left_object: 'A',
+          left_empty: ' ',
+          left_arrow: '',
+          line: '--',
+          right_arrow: '>',
+          right_empty: ' ',
+          right_object: 'B',
+          right_eol: ' : ABCD',
+        },
+      },
+    ]
+
+    test_pattern_containing_a_colon_for_comments.forEach(
+      ({ input_string, expect }) => {
+        const actual = parse_object_relation(input_string)
+        assert.deepEqual(actual, expect)
+      }
+    )
   })
 })
