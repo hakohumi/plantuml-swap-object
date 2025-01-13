b3233dd7eae4a1c6aadc52bca09b3bee0f0041ca
filu <mihumihu32@gmail.com>
2025-01-13 14:20:54 +0900

    🚧 ✨ add swap commands to package.json and extension activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,32 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
+  console.log(
+    'Congratulations, your extension "plantuml-swap-object" is now active!'
+  )
 
-	console.log('Congratulations, your extension "plantuml-swap-object" is now active!');
+  const swap_object_disposable = vscode.commands.registerCommand(
+    'plantuml-swap-object.swapObject',
+    () => {
+      console.log('swap object')
+    }
+  )
+  const swap_arrow_disposable = vscode.commands.registerCommand(
+    'plantuml-swap-object.swapArrow',
+    () => {
+      console.log('swap arrow')
+    }
+  )
+  const swap_object_and_arrow_disposable = vscode.commands.registerCommand(
+    'plantuml-swap-object.swapObjectAndArrow',
+    () => {
+      console.log('swap object and arrow')
+    }
+  )
 
-	const disposable = vscode.commands.registerCommand('plantuml-swap-object.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from plantuml-swap-object!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(swap_object_disposable)
+  context.subscriptions.push(swap_arrow_disposable)
+  context.subscriptions.push(swap_object_and_arrow_disposable)
 }
 
 export function deactivate() {}
